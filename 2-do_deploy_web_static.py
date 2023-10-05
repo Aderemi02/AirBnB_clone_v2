8caca99fa6a6f370966c117d15b2a9871dcc1347
Aderemi <omoremilekun@gmail.com>
2023-10-05 10:36:54 -0700

    deploying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,17 +15,16 @@ def do_deploy(archive_path):
     if not exists(archive_path):
         return False
     try:
-        arcFile = archive_path.split("/")[-1]
-        arcFile_no_ext = arcFile.split(".")[0]
-        put(archive_path, '/tmp/')
+        arcFile = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp/")
         loc = "/data/web_static/releases/"
-        run('mkdir -p {}{}/'.format(loc, arcFile_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(arcFile, loc, arcFile_no_ext))
-        run('rm -rf /tmp/{}'.format(arcFile))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(loc, arcFile_no_ext))
-        run('rm -rf {}{}/web_static'.format(loc, arcFile_no_ext))
+        run('mkdir -p {}{}/'.format(loc, arcFile))
+        run('tar -xzf /tmp/{}.tgz -C {}{}/'.format(arcFile, loc, arcFile))
+        run('rm -rf /tmp/{}.tgz'.format(arcFile))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(loc, arcFile))
+        run('rm -rf {}{}/web_static'.format(loc, arcFile))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(loc, arcFile_n_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(loc, arcFile))
         return True
     except Exception:
         return False
