d7ddaf81575c92e284ce6f609e76758fbd0b18e7
Aderemi <omoremilekun@gmail.com>
2023-10-05 09:47:54 -0700

    deploying archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
 """
-a Fabric script (based on the file 1-pack_web_static.py) that distributes an
-archive to your web servers, using the function do_deploy
+a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy
 """
 
 from os.path import exists
 from fabric.api import put, env, run
-env.hosts = ['52.3.220.197', '54.82.207.84']
+env.hosts = ['100.26.154.225', '54.209.218.5']
 
 
 def do_deploy(archive_path):
-    """scripty that distributes an archive to web servers"""
+    """this distributes an archive to the web servers"""
     if not exists(archive_path):
-        print("Not exist")
         return False
     try:
-        a_file = archive_path.split("/")[-1]
-        f_name = a_file.split(".")[0]
-        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, f_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(a_file, path, f_name))
-        run('rm /tmp/{}'.format(a_file))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, f_name))
-        run('rm -rf {}{}/web_static'.format(path, f_name))
+        arcFilename = archive_path.split("/")[-1]
+        arcFilename_no_ext = arcFilename.split(".")[0]
+        loc = "/data/web_static/releases/"
+        run('mkdir -p {}{}/'.format(loc, arcFilename_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(arcFilename, loc, arcFilename_no_ext))
+        run('rm /tmp/{}'.format(arcFilename))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(arcFilename, arcFilename_no_ext))
+        run('rm -rf {}{}/web_static'.format(arcFilename, arcFilename_no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, f_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(loc, arcFilename_n_ext))
         return True
-    except Exception as e:
-        print("except {}".format(e))
+    except:
         return False
