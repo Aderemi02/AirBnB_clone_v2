26307f00d6bd8ace4ba06ef304369c24da9aac68
Aderemi <omoremilekun@gmail.com>
2023-10-05 08:22:06 -0700

    setting up server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get -y update
 sudo apt-get -y ugrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+echo "this is preparing the server" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
