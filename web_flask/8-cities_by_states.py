470e4489c1842689e66bd3da5be76d317bef2b96
Aderemi <omoremilekun@gmail.com>
2023-10-22 16:33:34 -0700

    list of states and its cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,10 +12,10 @@ app = Flask(__name__)
 """flask web instance"""
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     """displays state lists with id"""
-    states = list(storage.all("State").values())
+    states = storage.all("State").values()
     return render_template("8-cities_by_states.html", states=states)
 
 
