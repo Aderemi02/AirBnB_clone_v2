d46dbd596a6eabb47f363ab4bda919312fed5953
Aderemi <omoremilekun@gmail.com>
2023-10-22 11:46:17 -0700

    flask app

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,11 +34,10 @@ def python(text="is cool"):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<n>", strict_slashes=False)
-def python(n):
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
     """displays number only if integer"""
-    if int(n):
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
