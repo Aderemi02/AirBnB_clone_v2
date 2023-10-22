8e845274d877d1da85c5fca3476561de1701399b
Aderemi <omoremilekun@gmail.com>
2023-10-22 11:13:15 -0700

    flask app

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,7 +15,7 @@ def index():
     return "Hello HBNB!"
 
 
-@app.route("/HBNB", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """displays HBNB"""
     return "HBNB"
